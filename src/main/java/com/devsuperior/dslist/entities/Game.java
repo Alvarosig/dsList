891e18f45f36631640ad3937fd0c8c4d6fea2319
Alvaro Silva <alvaro_s.g@hotmail.com>
2023-07-10 22:29:33 -0300

    Seed games

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -20,15 +20,20 @@ import lombok.NoArgsConstructor;
 public class Game {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	private Double score;
+	
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 }
