62b3750493db651d8f49699276ce79c27f883460
Alvaro Silva <alvaro_s.g@hotmail.com>
2023-07-11 22:13:58 -0300

    Criado nova entity gameList com repository, service e DTO

--- src/main/java/com/devsuperior/dslist/entities/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/entities/dto/GameDTO.java
@@ -1,25 +1,30 @@
 package com.devsuperior.dslist.entities.dto;
 
+import org.springframework.beans.BeanUtils;
+
 import com.devsuperior.dslist.entities.Game;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Getter
 @NoArgsConstructor
-public class GameMinDTO {
+@Getter
+@Setter
+public class GameDTO {
 	
 	private Long id;
 	private String title;
+	private Double score;
 	private Integer year;
+	private String genre;
+	private String platforms;
 	private String imgUrl;
 	private String shortDescription;
+	private String longDescription;
 	
-	public GameMinDTO(Game entity) {
-		id = entity.getId();
-		title = entity.getTitle();
-		year = entity.getYear();
-		imgUrl = entity.getImgUrl();
-		shortDescription = entity.getShortDescription();
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
 	}
+	
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.entities.dto.GameDTO;
 import com.devsuperior.dslist.entities.dto.GameMinDTO;
 import com.devsuperior.dslist.repository.GameRepository;
+
 
 @Service
 public class GameService {
@@ -15,6 +18,13 @@ public class GameService {
 	@Autowired
 	private GameRepository repository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById (Long id) {
+		Game result = repository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll () {
 		List<Game> result = repository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
