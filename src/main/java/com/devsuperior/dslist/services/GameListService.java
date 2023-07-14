24455206585fb3083db20f6ec8f41a21fbc41833
Alvaro Silva <alvaro_s.g@hotmail.com>
2023-07-13 22:40:49 -0300

    Adicionado replacementDTO para manipulação da lista de games e implementacao logica

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.entities.GameList;
 import com.devsuperior.dslist.entities.dto.GameListDTO;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameListRepository;
+import com.devsuperior.dslist.repository.GameRepository;
 
 
 @Service
@@ -17,10 +19,27 @@ public class GameListService {
 	@Autowired
 	private GameListRepository repository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll () {
 		List<GameList> result = repository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	}
 	
+	@Transactional
+	public void move (Long listId, int sourceIndex, int destinationIndex) {
+		List <GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			repository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+	}
 }
